4bbed7345f817173ec2d94a46eb1102c92cdb402
WonderFIeld <o.mildzikhov@mail.ru>
2021-01-08 01:40:28 +0300

    fix all isues

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,27 @@
 'use strict'
-const profileHeader = document.querySelector('.profile__header'),
-    profileHeaderText = document.querySelector('.profile__text'),
-    profileSubtitle = document.querySelector('.profile__subtitle');
 
-const closeBtn = document.querySelector('.popup__button_close');
-const editBtn = document.querySelector('.profile__button_edit');
-const saveBtn = document.querySelector('.popup__button_save');
-const popup = document.querySelector('.popup');
-const profileHeaderInput = document.querySelector('#profile__header-change');
-const profileSubtitleInput = document.querySelector('#profile__subtitle-change');
-const likeBtn = document.querySelectorAll('.grid__button');
+let formElement = popup.querySelector('.popup__form'),
+    profileHeader = document.querySelector('.profile__header'),
+    profileSubtitle = document.querySelector('.profile__subtitle'),
+    closeBtn = document.querySelector('.popup__button_close'),
+    editBtn = document.querySelector('.profile__button_edit'),
+    popup = document.querySelector('.popup'),
+    profileHeaderInput = document.querySelector('#profile__header-change'),
+    profileSubtitleInput = document.querySelector('#profile__subtitle-change');
 
+function changeVisablePopup() {
+    popup.classList.toggle('popup_active');
+    profileHeaderInput.value = profileHeader.innerText;
+    profileSubtitleInput.value = profileSubtitle.innerText;
+}
 
-profileHeaderInput.value = profileHeaderText.innerText;
-profileSubtitleInput.value = profileSubtitle.innerText;
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileHeader.innerText = profileHeaderInput.value
+    profileSubtitle.innerText = profileSubtitleInput.value
+    changeVisablePopup();
+}
 
-editBtn.addEventListener('click', ()=> {
-    console.log('hi', popup)
-    popup.classList.add('popup_active');
-})
-
-closeBtn.addEventListener('click', ()=> {
-    popup.classList.remove('popup_active');
-});
-
-saveBtn.addEventListener('click', () => {
-    profileHeaderText.innerText = profileHeaderInput.value;
-    // profileHeader.insertAdjacentElement('beforeend', editBtn);
-    // profileSubtitle.innerText = profileSubtitleInput.value;
-    popup.classList.remove('popup_active');
-})
-
-likeBtn.forEach((item) => {
-    item.addEventListener('click', ()=> {
-        item.classList.toggle('grid__button_like')
-    })
-})
+editBtn.addEventListener('click', changeVisablePopup);
+closeBtn.addEventListener('click', changeVisablePopup);
+formElement.addEventListener('submit', handleFormSubmit);
